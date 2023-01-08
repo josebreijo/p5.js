be80c8e17ca97bcb5e555491c48d45e5908209d0
José Breijo <josejbreijo99@gmail.com>
2023-01-07 20:26:18 -0600

    chore: separate vendor chunks

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 
+import { splitVendorChunkPlugin } from 'vite';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [preact(), splitVendorChunkPlugin()],
   build: {
     sourcemap: true,
   },
